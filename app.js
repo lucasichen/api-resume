fd8102ddda3af094735a2797b3898a909e9f57f0
Lucas Chen <lucas.chen2016@gmail.com>
2023-10-15 15:27:42 -0400

    feat: add rate limiting

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+const rateLimit = require('express-rate-limit');
+const requestIp = require('request-ip');
 const mongoose = require('mongoose');
 const { unsupportedMethodHandler } = require('./middleware/easterEgg');
 const db = require('./config/connection');
@@ -24,14 +26,21 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(requestIp.mw());
+app.use(rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 10, // limit each IP to 10 requests per windowMs
+  keyGenerator: (req, res) => {
+    return req.clientIp // IP address from requestIp.mw(), as opposed to req.ip
+  }
+}));
 // Apply the unsupportedMethodHandler middleware to all routes
-// app.use('/api/', unsupportedMethodHandler, routeRouter);
-// app.use('/api/experience', unsupportedMethodHandler, experienceRouter);
-// app.use('/api/projects', unsupportedMethodHandler, projectsRouter);
-// app.use('/api/education', unsupportedMethodHandler, educationRouter);
-// app.use('/api/contact', unsupportedMethodHandler, contactRouter);
-// app.use('/api/resume', unsupportedMethodHandler, meRouter);
+app.use('/api/', unsupportedMethodHandler, routeRouter);
+app.use('/api/experience', unsupportedMethodHandler, experienceRouter);
+app.use('/api/projects', unsupportedMethodHandler, projectsRouter);
+app.use('/api/education', unsupportedMethodHandler, educationRouter);
+app.use('/api/contact', unsupportedMethodHandler, contactRouter);
+app.use('/api/resume', unsupportedMethodHandler, meRouter);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + "/public/index.html"));
